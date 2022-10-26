593e8068c7c4b2c3fbdbe21dbbaf9945c8f86af0
Yuval Suede <yuvalsuede@gmail.com>
2022-10-26 09:44:19 +0700

    update autuplay video

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,143 +1,132 @@
-import React, { useState, useEffect } from 'react';
+import React, {useState, useEffect} from 'react';
 // import Hidden from '@material-ui/core/Hidden';
 // import { makeStyles } from '@material-ui/core/styles';
 import Section from 'components/Section';
 // import YoutubeEmbed from '../components/YoutubeEmbed';
-import { MenuDrawer } from './Drawer/MenuDrawer/MenuDrawer';
-import { InformationDrawer } from './Drawer/InformationDrawer/InformationDrawer';
-import { NewsFlashesDrawer } from './Drawer/NewsFlashesDrawer/NewsFlashesDrawer';
+import {MenuDrawer} from './Drawer/MenuDrawer/MenuDrawer';
+import {InformationDrawer} from './Drawer/InformationDrawer/InformationDrawer';
+import {NewsFlashesDrawer} from './Drawer/NewsFlashesDrawer/NewsFlashesDrawer';
 import HeaderBar from 'components/HeaderBar';
 import FirstShowFlag from 'components/FirstShowFlag';
 import VideoText from 'components/VideoText';
 import Vimeo from '@u-wave/react-vimeo';
-import { useWindowWidth } from '@react-hook/window-size';
+import {useWindowWidth} from '@react-hook/window-size';
 
 // const useStyles = makeStyles((theme) => ({
 
 // }));
 
 function Navbar(props) {
-  const [menuDrawerOpen, setMenuDrawerOpen] = useState(false);
-  const [informationDrawerOpen, setInformationDrawerOpen] = useState(false);
-  const [newsFlashesDrawerOpen, setNewsFlashesDrawerOpen] = useState(false);
-  const windowWidth = useWindowWidth();
-  const isDesktopLayout = windowWidth >= 960;
+    const [menuDrawerOpen, setMenuDrawerOpen] = useState(false);
+    const [informationDrawerOpen, setInformationDrawerOpen] = useState(false);
+    const [newsFlashesDrawerOpen, setNewsFlashesDrawerOpen] = useState(false);
+    const windowWidth = useWindowWidth();
+    const isDesktopLayout = windowWidth >= 960;
 
-  const {
-    menuItems,
-    newsFlashesItems,
-    showsItems,
-    faqItems,
-    mainData,
-    ticketsInformationData,
-    socialIcons,
-  } = props;
+    const {
+        menuItems,
+        newsFlashesItems,
+        showsItems,
+        faqItems,
+        mainData,
+        ticketsInformationData,
+        socialIcons,
+    } = props;
 
-  const {
-    mainVideo,
-    mainVideoMobile,
-    flagText,
-    firstShowDate,
-    flagButtonText,
-    showLink,
-    videoBigText,
-    videoSmallText,
-    ticketsInformationText,
-    ticketsInformationLink,
-    allNewsFlashText,
-    allNewsFlashLink,
-  } = mainData;
+    const {
+        mainVideo,
+        mainVideoMobile,
+        flagText,
+        firstShowDate,
+        flagButtonText,
+        showLink,
+        videoBigText,
+        videoSmallText,
+        ticketsInformationText,
+        ticketsInformationLink,
+        allNewsFlashText,
+        allNewsFlashLink,
+    } = mainData;
 
-  const handleMenuDrawerItemClick = () => {
-    setMenuDrawerOpen(false);
-  };
+    const handleMenuDrawerItemClick = () => {
+        setMenuDrawerOpen(false);
+    };
 
-  useEffect(() => {
+    useEffect(() => {
 
-  }, [isDesktopLayout])
+    }, [isDesktopLayout])
 
-  // const videoId = video?.url?.substring(video?.url.indexOf('=') + 1);
+    // const videoId = video?.url?.substring(video?.url.indexOf('=') + 1);
 
-  return (
-    <Section
-      size='auto'
-      sx={{
-        // minHeight: '796px',
-      }}>
-      {/* <YoutubeEmbed
-        // url={'iCAKpASnFgw'}
-        url={videoId}
-        autoplay={true}
-        width={'100%'}
-        height={'796px'}
-        containerStyle={{
-          margin: '0px auto',
-          position: 'absolute',
-          minHeight: '796px',
-        }}
-      /> */}
-      <Vimeo
-        video={isDesktopLayout ? mainVideo?.url : mainVideoMobile?.url}
-        showTitle={false}
-        loop={true}
-        controls={true}
-        autoplay={true}
-        playsInline={true}
-        height='796px'
-        width={'100%'}
-        // volume={1}
-        // paused={false}
-        // muted={false}
-        // background={true}
-        responsive={true}
-        style={{
-          margin: '0px auto',
-          position: 'relative',
-          // minHeight: '796px',
-          width: '100%',
-        }}
-      />
-      <FirstShowFlag
-        flagButtonText={flagButtonText}
-        showLink={showLink}
-        flagText={flagText}
-        firstShowDate={firstShowDate}
-      />
-      <VideoText videoBigText={videoBigText} videoSmallText={videoSmallText} />
-      
-      <HeaderBar
-      isDesktopLayout={isDesktopLayout}
-        socialIcons={socialIcons}
-        ticketsInformationText={ticketsInformationText}
-        setMenuDrawerOpen={setMenuDrawerOpen}
-        setInformationDrawerOpen={setInformationDrawerOpen}
-        setNewsFlashesDrawerOpen={setNewsFlashesDrawerOpen}
-      />
-      <MenuDrawer
-        items={menuItems}
-        handleItemClick={handleMenuDrawerItemClick}
-        ticketsInformationText={ticketsInformationText}
-        ticketsInformationLink={ticketsInformationLink}
-        drawerOpen={menuDrawerOpen}
-        setDrawerOpen={setMenuDrawerOpen}
-      />
-      <InformationDrawer
-        ticketsInformationData={ticketsInformationData}
-        showsItems={showsItems}
-        faqItems={faqItems}
-        ticketsInformationText={ticketsInformationText}
-        drawerOpen={informationDrawerOpen}
-        setDrawerOpen={setInformationDrawerOpen}
-      />
-      <NewsFlashesDrawer
-        items={newsFlashesItems}
-        allNewsFlashText={allNewsFlashText}
-        allNewsFlashLink={allNewsFlashLink}
-        drawerOpen={newsFlashesDrawerOpen}
-        setDrawerOpen={setNewsFlashesDrawerOpen}
-      />
-    </Section>
-  );
+    return (
+        <Section
+            size='auto'
+            sx={{
+                // minHeight: '796px',
+            }}>
+            <Vimeo
+                background
+                video={isDesktopLayout ? mainVideo?.url : mainVideoMobile?.url}
+                showTitle={false}
+                loop
+                controls
+                autoplay
+                playsInline
+                height='796px'
+                width={'100%'}
+                // volume={1}
+                // paused={false}
+                // muted={false}
+                // background={true}
+                responsive
+                style={{
+                    margin: '0px auto',
+                    position: 'relative',
+                    // minHeight: '796px',
+                    width: '100%',
+                }}
+            />
+            <FirstShowFlag
+                flagButtonText={flagButtonText}
+                showLink={showLink}
+                flagText={flagText}
+                firstShowDate={firstShowDate}
+            />
+            <VideoText videoBigText={videoBigText} videoSmallText={videoSmallText}/>
+
+            <HeaderBar
+                isDesktopLayout={isDesktopLayout}
+                socialIcons={socialIcons}
+                ticketsInformationText={ticketsInformationText}
+                setMenuDrawerOpen={setMenuDrawerOpen}
+                setInformationDrawerOpen={setInformationDrawerOpen}
+                setNewsFlashesDrawerOpen={setNewsFlashesDrawerOpen}
+            />
+            <MenuDrawer
+                items={menuItems}
+                handleItemClick={handleMenuDrawerItemClick}
+                ticketsInformationText={ticketsInformationText}
+                ticketsInformationLink={ticketsInformationLink}
+                drawerOpen={menuDrawerOpen}
+                setDrawerOpen={setMenuDrawerOpen}
+            />
+            <InformationDrawer
+                ticketsInformationData={ticketsInformationData}
+                showsItems={showsItems}
+                faqItems={faqItems}
+                ticketsInformationText={ticketsInformationText}
+                drawerOpen={informationDrawerOpen}
+                setDrawerOpen={setInformationDrawerOpen}
+            />
+            <NewsFlashesDrawer
+                items={newsFlashesItems}
+                allNewsFlashText={allNewsFlashText}
+                allNewsFlashLink={allNewsFlashLink}
+                drawerOpen={newsFlashesDrawerOpen}
+                setDrawerOpen={setNewsFlashesDrawerOpen}
+            />
+        </Section>
+    );
 }
 
 export default Navbar;
--- src/components/VideoText.js
+++ src/components/VideoText.js
@@ -1,38 +1,41 @@
 import Box from '@material-ui/core/Box';
 
 export const VideoText = (props) => {
-  const { videoBigText, videoSmallText } = props;
+    const {videoBigText, videoSmallText} = props;
 
-  return (
-    <Box
-      sx={{
-        opacity: '0.7',
-        position: 'absolute',
-        zIndex: '10',
-        bottom: '50px',
-        right: '0',
-        left: '0',
-        textAlign: 'center',
-        fontFamily: 'Noto Sans Hebrew',
-        textShadow: '0px 3px 14px #0000005c',
-      }}>
-      <Box
-        sx={{
-          color: '#fff',
-          fontSize: '4.467vw',
-          fontWeight: 'bold',
-        }}>
-        {videoBigText}
-      </Box>
-      <Box
-        sx={{
-          color: '#FBD491',
-          fontSize: '2.207vw',
-        }}>
-        {videoSmallText}
-      </Box>
-    </Box>
-  );
+    return (
+        <Box
+            sx={{
+                opacity: '0.7',
+                position: 'absolute',
+                zIndex: '10',
+                bottom: '50px',
+                right: '0',
+                left: '0',
+                textAlign: 'center',
+                fontFamily: 'Noto Sans Hebrew',
+                textShadow: '0px 3px 14px #0000005c',
+            }}>
+            <Box
+                sx={{
+                    color: '#fff',
+                    fontSize: '4.467vw',
+                    fontWeight: 'bold',
+                    textShadow: '1px 6px 4px #000000',
+                }}>
+                {videoBigText}
+            </Box>
+            <Box
+                sx={{
+                    color: '#FBD491',
+                    fontSize: '2.207vw',
+                    textShadow: '1px 6px 4px #000000',
+
+                }}>
+                {videoSmallText}
+            </Box>
+        </Box>
+    );
 };
 
 export default VideoText;
